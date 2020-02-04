d8473934a7913356322feaba7734da57c17eeb60
qingwen.ye <yhl@1024hw.org>
2020-02-04 18:30:45 +0800

    添加参数校验中间件

--- packages/userbind/lib/router/index.js
+++ packages/userbind/lib/router/index.js
@@ -6,18 +6,16 @@ const ms = new MS()
 
 module.exports = function (service) {
   const router = ms.router()
-  const { userBind } = service
+  const { userBind, validatorMiddleware } = service
 
   async function getBindUser (opts) {
     const { id } = opts.params
-    if (!id) throw error.err(error.Err.FA_BADREQUEST)
     const bindId = await userBind.getBindUser(id)
     return { id, bindId }
   }
 
   async function getBoundUser (opts) {
     const { id: bindId } = opts.params
-    if (!bindId) throw error.err(error.Err.FA_BADREQUEST)
     const id = await userBind.getBoundUser(bindId)
     return { id, bindId }
   }
@@ -25,7 +23,7 @@ module.exports = function (service) {
   async function bindUser (opts) {
     const { params: { id }, data: { bindId } } = opts
 
-    if (!id || !bindId) throw error.err(error.Err.FA_BADREQUEST)
+    if (id === bindId) throw error.err(error.Err.FA_BADREQUEST)
 
     // 判断是否已经绑定或者被绑定
     const [idInBind, bindIdInBind] = await Promise.all([
@@ -39,14 +37,25 @@ module.exports = function (service) {
 
   async function unBindUser (opts) {
     const { id } = opts.params
-    if (!id) throw error.err(error.Err.FA_BADREQUEST)
     const bindId = await userBind.unBindUser(id)
     return { id, bindId }
   }
 
-  router.add('/binds/:id', 'get', getBindUser)
-  router.add('/bindeds/:id', 'get', getBoundUser)
-  router.add('/binds/:id', 'put', bindUser)
-  router.add('/binds/:id', 'delete', unBindUser)
+  router.add('/binds/:id', 'get', validatorMiddleware({
+    id: { type: 'string', min: 1, max: 500, optional: false }
+  }, getBindUser))
+
+  router.add('/bindeds/:id', 'get', validatorMiddleware({
+    id: { type: 'string', min: 1, max: 500, optional: false }
+  }, getBoundUser))
+
+  router.add('/binds/:id', 'put', validatorMiddleware({
+    id: { type: 'string', min: 1, max: 500, optional: false },
+    bindId: { type: 'string', min: 1, max: 500, optional: false }
+  }, bindUser))
+
+  router.add('/binds/:id', 'delete', validatorMiddleware({
+    id: { type: 'string', min: 1, max: 500, optional: false }
+  }, unBindUser))
   return router
 }
--- packages/userbind/lib/service/index.js
+++ packages/userbind/lib/service/index.js
@@ -1,5 +1,9 @@
 const config = require('config')
+const Validator = require('fastest-validator')
+const error = require('jm-err')
 const UserBind = require('./userbind')
+
+const validator = new Validator()
 
 module.exports = class extends require('service') {
   constructor (opts = {}) {
@@ -11,5 +15,26 @@ module.exports = class extends require('service') {
   router (opts) {
     const dir = require('path').join(__dirname, '../router')
     return new (require('router'))(this, { dir, ...opts }).router
+  }
+
+  /**
+   * 校验中间件
+   * @param {object} schema 校验定义
+   * @param {function} handler 函数
+   * @returns {function} -
+   */
+  validatorMiddleware (schema, handler) {
+    const check = validator.compile(schema)
+    return function validateParams (opts) {
+      const params = Object.assign(opts.params, opts.data)
+      const res = check(params)
+      if (res === true) {
+        if (handler) {
+          return handler(opts)
+        }
+      } else {
+        throw error.err({ err: 422, msg: 'Parameter Validation Error', data: res })
+      }
+    }
   }
 }
